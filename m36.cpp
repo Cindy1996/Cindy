00a01eb6bda8ddf2ec25ac73029fffcadd16a119
Cindy <31720400+Cindy1996@users.noreply.github.com>
2017-10-16 23:00:08 +0800

    Add files via upload

---
+++ m36.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+void main()
+{
+	int k,r,l,s,pi;
+	cout<<"ÇëÊäÈër,k";
+	cin>>r>>k;
+	pi=3.14159;
+	l=2*pi*r;
+	s=pi*r*r;
+	switch(k)
+	{
+	case 1:cout<<"l="<<l<<endl;break;
+	case 2:cout<<"s="<<s<<endl;break;
+	case 3:cout<<"l="<<"s="<<endl;break;
+	}
+}
+
+
+
