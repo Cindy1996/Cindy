5c81adfa204bf5ff85d58aac293ad1b5bb8eba45
Cindy <31720400+Cindy1996@users.noreply.github.com>
2017-10-16 23:00:27 +0800

    Add files via upload

---
+++ m38.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+void main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a << '/' << b << '=';
+    if ( a % b ==0 )
+        cout << a / b << endl;
+    else
+        cout << a / b << "..." << a % b << endl;
+}
